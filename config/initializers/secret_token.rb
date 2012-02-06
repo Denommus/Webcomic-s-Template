ecbf319d00edfe6a6139d3368dea780e2ea94cf9
Yuri Albuquerque <yuridenommus@gmail.com>
2012-02-06 11:32:00 -0400

    Basic rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webcomic::Application.config.secret_token = '448aa9bbf014e30b19664443c66b7a6776f89fed41574f36f3aa5bd782500bf9188353f29b63ccae130b2f1314be5933d4ca4d58a673dafb739badfd0c9974aa'
